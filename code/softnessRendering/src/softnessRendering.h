449423901a7934bbb9da93a44ff8a6fbc8607333
Mustafa Mete <a.mustafamete@gmail.com>
2023-12-01 17:03:22 +0100

    right-before-submission

--- code/softnessRendering/src/softnessRendering.h
+++
@@ -1,3 +0,0 @@
-#include <cmath>
-double softnessRenderingContactAreaModel (int userID = 0, int materialID = 0, double force = 0);
-double softnessRenderingStiffnessModel (int userID = 0, int materialID = 0, double displacement = 0);
